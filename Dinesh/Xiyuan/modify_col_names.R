2f0d410cde6dc29cc2a45741b512e37de9ea7fb5
Dinesh R Poddaturi <dinesh@iastate.edu>
2022-01-26 16:47:13 -0600

    Delete Dinesh/Xiyuan directory

--- Dinesh/Xiyuan/modify_col_names.R
+++
@@ -1,9 +0,0 @@
-library(readr)
-ks_gbm_predict <- read_csv("~/Dropbox/Spring 2018/Stat502/stat502_project/Xiyuan/ks_gbm_predict.csv")
-
-names(ks_gbm_predict) <- c('Id', 'price')
-write_csv(ks_gbm_predict, "new_ks_gbm_predict.csv")
-
-ks_knn_predict <- read_csv("~/Dropbox/Spring 2018/Stat502/stat502_project/Xiyuan/ks_knn_predict.csv")
-names(ks_knn_predict) <- c('Id', 'price')
-write_csv(ks_knn_predict, "new_ks_knn_predict.csv")
