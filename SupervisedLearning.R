eff5212aff5a0f0317296b66102231bd9bad335d
Dinesh R Poddaturi <dinesh@iastate.edu>
2022-01-30 11:47:32 -0600

    Some work added

--- SupervisedLearning.R
+++ SupervisedLearning.R
@@ -92,14 +92,14 @@ impVar_lm
 # 18:        floors 0.001008294 0.005542399 0.010651546
 
 #### I am also using Boruta feature selection algorithm to determine the variable importance
-# boruta_fit <- Boruta(price~. , data = train_data , doTrace = 2)
+boruta_fit <- Boruta(price~. , data = train_data , doTrace = 2)
 
-# print(boruta_fit)
+print(boruta_fit)
 # Boruta performed 68 iterations in 15.85281 mins.
 # 18 attributes confirmed important: bathrooms,
 # bedrooms, condition, floors, grade and 13 more;
 # No attributes deemed unimportant.
-# plot(boruta_fit)
+plot(boruta_fit)
 
 colMeans(boruta_fit$ImpHistory)
 
@@ -211,17 +211,101 @@ lmTune_housePrice <- train(housePrice~., data = house_train,
 lmTune_housePrice$results
 
 
-
 ########################################################################################################
 ####################################### USING RANDOM FOREST ############################################
 ########################################################################################################
+
+
 designMat <- model.matrix(lm(housePrice~.,data=house_train))
 designMat <- designMat[,-1]
 
 forestTune_housePrice <- train(y = house_train[,1], x = designMat,
-                             tuneGrid = data.frame(mtry=1:100),
-                             method = "rf", ntree = 500,
+                             tuneGrid = data.frame(mtry=1:50),
+                             method = "rf", ntree = 150,
                              trControl = trainControl(method="oob"))
+# In the Random Forest model, the original training data is randomly sampled-with-replacement generating small subsets of data.
+# These subsets are also known as bootstrap samples.
+# mtry: Number of variables randomly sampled as candidates at each split.
+# ntree: Number of trees to grow.
+# oob: Out Of Bag is a method to validate the ranndom forest model. 
+# At each bootstrap, the algorithm leaves out some rows and trains with kept data. Once the training is finished, the model is 
+# fed the left out rows and asks to predict the outcome. This is repeated multiple times and oob score is computed by simply adding
+# up the number of correctly predicted rows. 
+# We can also use repeated CV as well. However, OOB is a well known method and is recognized well. 
+
+plot(forestTune_housePrice)
+
+forestTune_housePrice
+# Random Forest 
+# 
+# 10000 samples
+# 85 predictor
+# 
+# No pre-processing
+# Resampling results across tuning parameters:
+#   
+#   mtry  RMSE      Rsquared 
+# 1    310202.1  0.3288181
+# 2    240160.1  0.5976978
+# 3    210520.7  0.6908705
+# 4    192123.4  0.7425391
+# 5    182811.1  0.7668927
+# 6    175636.4  0.7848309
+# 7    168967.7  0.8008603
+# 8    164429.1  0.8114147
+# 9    162614.4  0.8155541
+# 10    159146.9  0.8233364
+# 11    161349.2  0.8184131
+# 12    157665.6  0.8266097
+# 13    157255.1  0.8275116
+# 14    156379.6  0.8294267
+# 15    155123.8  0.8321553
+# 16    154627.6  0.8332274
+# 17    155400.4  0.8315563
+# 18    153610.1  0.8354149
+# 19    152152.2  0.8385243
+# 20    151776.8  0.8393202
+# 21    153545.8  0.8355527
+# 22    151808.8  0.8392523
+# 23    153067.8  0.8365751
+# 24    153282.6  0.8361160
+# 25    150461.3  0.8420935
+# 26    151271.3  0.8403886
+# 27    151569.5  0.8397586
+# 28    152608.6  0.8375542
+# 29    150119.1  0.8428108
+# 30    150416.8  0.8421867
+# 31    148686.7  0.8457963
+# 32    151615.5  0.8396614
+# 33    151846.9  0.8391717
+# 34    150312.1  0.8424064
+# 35    151660.1  0.8395671
+# 36    152666.3  0.8374313
+# 37    149208.4  0.8447122
+# 38    148171.3  0.8468633
+# 39    150219.5  0.8426005
+# 40    149958.4  0.8431472
+# 41    151272.2  0.8403867
+# 42    150159.3  0.8427267
+# 43    150927.0  0.8411143
+# 44    150592.5  0.8418178
+# 45    150772.8  0.8414389
+# 46    150083.7  0.8428850
+# 47    150755.1  0.8414761
+# 48    153218.2  0.8362538
+# 49    148974.5  0.8451987
+# 50    152212.0  0.8383974
+# 
+# RMSE was used to select the optimal model
+# using the smallest value.
+# The final value used for the model was mtry
+# = 38.
+
+forestTune_housePrice$bestTune
+#     mtry
+# 38   38
+
+
 
 ########################################################################################################
 ####################################### USING TREE #####################################################
@@ -229,10 +313,14 @@ forestTune_housePrice <- train(y = house_train[,1], x = designMat,
 
 TreeTune_housePricing <- train(y = house_train[,1], x = house_train[,2:13],
                              method = "rpart", tuneGrid = data.frame(cp = seq(from = .0001, to = .1, length.out = 50)),
-                             trControl = trainControl(method = "repeatedcv",repeats = 2, number = 10))
+                             trControl = trainControl(method = "repeatedcv",repeats = 50, number = 10))
 
 
+plot(TreeTune_housePricing)
 
+TreeTune_housePricing$bestTune
+# cp (complexity parameter)
+# 1 1e-04
 
 ########################################################################################################
 ####################################### USING Gradient Boosting Machine ################################
@@ -244,7 +332,7 @@ gbm.grid <- expand.grid(n.trees = seq(from = 120, to = 160, length.out = 3),
 
 cv.control_house <- trainControl(method = "repeatedcv", repeats = 2, number = 10)
 
-GbmTune_housePricing<-train(y = house_train[,1], x = house_train[,2:13], tuneGrid = gbm.grid,
+GbmTune_housePricing <- train(y = house_train[,1], x = house_train[,2:13], tuneGrid = gbm.grid,
                             method = "gbm",
                             trControl = cv.control_house)
 
